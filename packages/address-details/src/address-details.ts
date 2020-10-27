cc51f0708b16e32d2f42ab484835ba2591847064
drizzer14 <drizzer14@gmail.com>
2020-10-27 15:57:55 +0200

    feat(address-details): Correct `AddressDetails` fields' names

--- packages/address-details/src/address-details.ts
+++ packages/address-details/src/address-details.ts
@@ -7,28 +7,28 @@ import { Type } from 'class-transformer';
 import { Classification } from '@ts4ocds/core/classification';
 
 export class AddressDetails<
-  CD extends string = string,
-  RD extends string = string,
-  LD extends string = string,
+  C extends string = string,
+  R extends string = string,
+  L extends string = string,
   NC extends string = string
 > {
   /**
    * Country code according to ['iso-alpha2'](https://www.iso.org/obp/ui/#search) classifier
    */
   @Type(() => Classification)
-  public countryDetails?: Classification<CD>;
+  public country?: Classification<C>;
 
   /**
    * Region code according to ['iso-alpha2'](https://www.iso.org/obp/ui/#search) classifier
    */
   @Type(() => Classification)
-  public regionDetails?: Classification<RD>;
+  public region?: Classification<R>;
 
   /**
    * Locality code according to either ['UN/LOCDE'](https://www.unece.org/cefact/locode/service/location.html) or any applicable local classifier
    */
   @Type(() => Classification)
-  public localityDetails?: Classification<LD>;
+  public locality?: Classification<L>;
 
   /**
    * The most detailed level of NUTS codes, NUTS 3 should be used and it should be taken from the EU codelist linked from
