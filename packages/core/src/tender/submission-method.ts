490fba5aef27730614f1dc3bd8ccf378d2a2a083
drizzer14 <drizzer14@gmail.com>
2020-10-27 14:58:03 +0200

    refactor(core): Formatting

--- packages/core/src/tender/submission-method.ts
+++ packages/core/src/tender/submission-method.ts
@@ -9,12 +9,7 @@ import { CodeList, OpenCodeList } from '@ts4ocds/utils';
  * The methods by which bids are submitted,
  * using the open [submissionMethod](https://standard.open-contracting.org/1.1/en/schema/codelists/#submission-method) codelist.
  */
-export const SubmissionMethods = new OpenCodeList([
-  'electronicSubmission',
-  'electronicAuction',
-  'written',
-  'inPerson',
-]);
+export const SubmissionMethods = new OpenCodeList(['electronicSubmission', 'electronicAuction', 'written', 'inPerson']);
 
 /**
  * The methods by which bids are submitted,
