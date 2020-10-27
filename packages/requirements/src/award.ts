63fb6492e92ee721b54f2abf329cbd81e0e1fc9f
drizzer14 <drizzer14@gmail.com>
2020-10-27 18:21:07 +0200

    fix(requirements): Remove excess `Type` decorator type function

--- packages/requirements/src/award.ts
+++ packages/requirements/src/award.ts
@@ -6,33 +6,12 @@
 import { Type } from 'class-transformer';
 import * as Core from '@ts4ocds/core/award';
 
-import {
-  RequirementResponse,
-  StringRequirementResponse,
-  BooleanRequirementResponse,
-  NumericRequirementResponse,
-} from './requirement-response';
+import { RequirementResponse } from './requirement-response';
 
 export class Award<RR extends RequirementResponse = RequirementResponse> extends Core.Award {
   /**
    * A list of the detailed responses of this award to the requirements of the tender.
    */
-  @Type((options) => {
-    switch (typeof (options?.object as Award | undefined)?.requirementResponses?.[0].value) {
-      case 'string': {
-        return StringRequirementResponse;
-      }
-      case 'boolean': {
-        return BooleanRequirementResponse;
-      }
-      case 'number': {
-        return NumericRequirementResponse;
-      }
-      case 'undefined':
-      default: {
-        return RequirementResponse;
-      }
-    }
-  })
+  @Type(() => RequirementResponse)
   public requirementResponses?: RR[];
 }
