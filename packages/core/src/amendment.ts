c53ca610827bb4f35088c24f5188ab0d1e4e454a
drizzer14 <drizzer14@gmail.com>
2020-10-27 10:18:53 +0200

    feat(core): Add `Amendment`

---
+++ packages/core/src/amendment.ts
@@ -0,0 +1,41 @@
+/**
+ * @packageDocumentation
+ * @module Amendment
+ */
+
+/**
+ * Amendment information
+ */
+export class Amendment {
+  /**
+   * The date of this amendment.
+   */
+  public date?: Date | string;
+
+  /**
+   * An explanation for the amendment.
+   */
+  public rationale?: string;
+
+  /**
+   * An identifier for this amendment: often the amendment number
+   */
+  public id?: string;
+
+  /**
+   * A free text, or semi-structured, description of the changes made in this amendment.
+   */
+  public description?: string;
+
+  /**
+   * Provide the identifier (release.id) of the OCDS release (from this contracting process)
+   * that provides the values for this contracting process **before** the amendment was made.
+   */
+  public amendsReleaseID?: string;
+
+  /**
+   * Provide the identifier (release.id) of the OCDS release (from this contracting process)
+   * that provides the values for this contracting process **after** the amendment was made.
+   */
+  public releaseID?: string;
+}
