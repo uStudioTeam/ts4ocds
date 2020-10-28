bae33482ec72b7a446522caf1438b8719b02952e
Dmytro Vasylkivskyi <drizzer14@gmail.com>
2020-10-28 14:07:55 +0200

    feat: Add `Metrics` package (#5)

---
+++ packages/metrics/src/metric.ts
@@ -0,0 +1,40 @@
+/**
+ * @packageDocumentation
+ * @module Metrics
+ */
+
+import { Type } from 'class-transformer';
+
+import { Observation } from './observation';
+
+/**
+ * Metrics are used to set out targets and results from a contracting process.
+ * During the planning and tender sections, a metric indicates the anticipated results.
+ * In award and contract sections it indicates the awarded/contracted results.
+ * In the implementation section it is used to provide updates on actually delivered results, also known as outputs.
+ */
+export class Metric<O extends Observation = Observation> {
+  /**
+   * An array of target or actual values for this metric.
+   */
+  @Type(() => Observation)
+  public observations: O[];
+
+  /**
+   * An identifier for this metric.
+   * In some cases this may be drawn from a codelist of metrics required for this type of contracting process,
+   * or in other instances may be an arbitrary identifier.
+   */
+  public id: string;
+
+  /**
+   * The title of this metric
+   */
+  public title?: string;
+
+  /**
+   * A short description of the metric.
+   * This may include short details of measurement methods.
+   */
+  public description?: string;
+}
---
+++ packages/metrics/src/observation/index.ts
@@ -0,0 +1,4 @@
+export * from './dimensions';
+export * from './observation';
+export * from './financial-observation';
+export * from './non-financial-observation';
