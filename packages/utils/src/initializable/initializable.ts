cfbb252c959f29cd8057b10b65fcc4e9030b5e13
drizzer14 <drizzer14@gmail.com>
2020-11-03 22:53:32 +0200

    feat(utils): Add TSDoc comments

--- packages/utils/src/initializable/initializable.ts
+++ packages/utils/src/initializable/initializable.ts
@@ -5,6 +5,10 @@
 
 import type { Initializer } from './initializer';
 
+/**
+ * The superclass that grants all its children the ability to be initialized through a constructor argument
+ * that is the object of the required shape.
+ */
 export abstract class Initializable<T> {
   public constructor(initializer: Initializer<T>) {
     Object.assign(this, initializer);
