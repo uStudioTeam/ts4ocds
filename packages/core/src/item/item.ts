039dfd67ef0a43314510535241ac0d4f9558e4b8
drizzer14 <drizzer14@gmail.com>
2020-11-03 23:35:07 +0200

    feat(core): Add classification scheme generic type for `Item`

--- packages/core/src/item/item.ts
+++ packages/core/src/item/item.ts
@@ -4,6 +4,7 @@
  */
 
 import { Initializable } from '@ts4ocds/utils';
+import { ItemClassificationScheme } from './item-classification-scheme';
 
 import type { Unit } from './unit';
 import type { Classification } from '../classification';
@@ -11,7 +12,7 @@ import type { Classification } from '../classification';
 /**
  * A good, service, or work to be contracted.
  */
-export class Item extends Initializable<Item> {
+export class Item<S extends ItemClassificationScheme = ItemClassificationScheme> extends Initializable<Item> {
   /**
    * A local identifier to reference and merge the items by.
    * Must be unique within a given array of items.
@@ -26,12 +27,12 @@ export class Item extends Initializable<Item> {
   /**
    * The primary {@link Classification | classification} for the item.
    */
-  public classification?: Classification;
+  public classification?: Classification<S | string>;
 
   /**
    * An array of additional {@link Classification | classifications} for the item.
    */
-  public additionalClassifications?: Classification[];
+  public additionalClassifications?: Classification<S | string>[];
 
   /**
    * A description of the {@link Unit | unit} in which the supplies, services or works are provided (e.g. hours, kilograms) and the unit-price.
