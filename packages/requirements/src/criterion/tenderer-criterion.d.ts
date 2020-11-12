6ef22a17db38229059acb11351321fd59fb50dfe
drizzer14 <drizzer14@gmail.com>
2020-11-12 15:18:34 +0200

    docs(requirements): update links

--- packages/requirements/src/criterion/tenderer-criterion.d.ts
+++ packages/requirements/src/criterion/tenderer-criterion.d.ts
@@ -11,9 +11,9 @@ import type { Criterion } from './criterion';
  * A criterion on which either bidders or {@link Item.Item | items} will be judged, evaluated or assessed.
  *
  * @param RG
- * A list of {@link RequirementGroup.RequirementGroup | requirement groups} for this criterion.
- * A criterion is satisfied by one or more {@link RequirementGroup.RequirementGroup | requirement groups} being met.
- * A {@link RequirementGroup.RequirementGroup | requirement groups} is met when all {@link Requirement.Requirement | requirements} in the group are satisfied.
+ * A list of {@link RequirementGroup | requirement groups} for this criterion.
+ * A criterion is satisfied by one or more {@link RequirementGroup | requirement groups} being met.
+ * A {@link RequirementGroup | requirement groups} is met when all {@link Requirement.Requirement | requirements} in the group are satisfied.
  */
 export interface TendererCriterion<RG extends RequirementGroup = RequirementGroup> extends Criterion<RG> {
   /**
